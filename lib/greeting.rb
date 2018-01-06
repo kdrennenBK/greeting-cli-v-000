ab40a33a9bbb223ca8c2076e31d30fd2b55b068f
Kate Drennen <k8ted11@gmail.com>
2018-01-06 21:01:11 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,3 @@
-def greeting(name)
-  puts "Hello, #{name}, what's your name?"
-end# code the #greeting method here!
+# def greeting(name)
+#   puts "Hello, #{name}, what's your name?"
+# end# code the #greeting method here!
